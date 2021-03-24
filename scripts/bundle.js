bf74634544d94e51277d5d82184482cf439ee693
Denis Pushkarev <zloirock@zloirock.ru>
2021-03-24 20:48:33 +0700

    add `summary` option to `@core-js/builder`

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,22 +1,10 @@
 'use strict';
-const { cyan, green } = require('chalk');
 const builder = require('@core-js/builder');
 const actual = require('@core-js/compat/entries')['core-js/actual'];
 
-const PATH = './packages/core-js-bundle/';
+const BUNDLE_PATH = './packages/core-js-bundle/';
 
-function log(kind, name, code) {
-  const size = (code.length / 1024).toFixed(2);
-  // eslint-disable-next-line no-console -- output
-  console.log(green(`${ kind }: ${ cyan(`${ PATH }${ name }.js`) }, size: ${ cyan(`${ size }KB`) }`));
-}
-
-async function bundle({ name, ...options }) {
-  const source = await builder({ filename: `${ PATH }${ name }.js`, ...options });
-  log('bundling', name, source);
-}
-
-bundle({ name: 'full', minify: false });
-bundle({ name: 'full.min' });
-bundle({ name: 'actual', modules: actual, minify: false });
-bundle({ name: 'actual.min', modules: actual });
+builder({ filename: `${ BUNDLE_PATH }full.js`, minify: false, summary: { size: true } });
+builder({ filename: `${ BUNDLE_PATH }full.min.js`, summary: { size: true } });
+builder({ filename: `${ BUNDLE_PATH }actual.js`, modules: actual, minify: false, summary: { size: true } });
+builder({ filename: `${ BUNDLE_PATH }actual.min.js`, modules: actual, summary: { size: true } });
