4c0c97277046045b3e1bdb91986ea1bdff796e5e
Denis Pushkarev <zloirock@zloirock.ru>
2021-04-19 13:15:12 +0700

    start to play with the service

--- .eslintrc.js
+++ .eslintrc.js
@@ -1003,6 +1003,8 @@ module.exports = {
         // packages
         'packages/core-js-builder/**',
         'packages/core-js-compat/**',
+        'packages/core-js-service/**',
+        'packages/core-js-ua-normalizer/**',
         // dev
         'scripts/**',
         'tests/observables/**',
